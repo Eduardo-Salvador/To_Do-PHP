c54d3bea69a4fbf453d3fcf9d3f2f6c0d0881f3f
EduardoNS1 <eduardonsantiago313@gmail.com>
2025-10-13 07:31:42 -0300

    feat(admin-backend): Start implementing the admin screen backend logic

--- Controller/admin.php
+++ Controller/admin.php
@@ -2,7 +2,15 @@
     session_start();
     include __DIR__ . "/../Model/connection.php";
 
-    
+    if (!isset($_SESSION['id_user'])) {
+    // Se não estiver logado, redireciona para a página de login
+        header('Location: ../Controller/Login/login.html');
+        exit();
+    }
+
+    $res = $conn->prepare("SELECT username, email, birthdate FROM user ORDER BY username ASC");
+    $res->execute();
+    $result = $res->get_result();
 
 
 ?>
